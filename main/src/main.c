8166d7a5b1a0e95e1c97b339e51b9b0753b90952
Vinh Nguyen <ngtv0404@gmail.com>
2025-09-29 16:34:55 +1000

    initial commit

---
+++ main/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esp_log.h"
+
+#define TAG                 "main" // log tag
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Hello, World!"); // TODO
+
+    while (true);
+}
